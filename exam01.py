7180faa64ed376a9cc6ee9456d920729242e9f46
KIMMINJAE <innersme@gmail.com>
2017-06-29 20:38:02 +0900

    170628

---
+++ exam01.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 28 16:42:25 2017
+
+@author: user
+"""
+
+num_1 = 123456
+
+num_2 = num_1 // 10
+num_3 = num_2 // 10
+num_4 = num_3 // 10
+num_5 = num_4 // 10
+num_6 = num_5 // 10
+
+
+print(num_1%10)
+print(num_2%10)
+print(num_3%10)
+print(num_4%10)
+print(num_5%10)
+print(num_6%10)
---
+++ exam03.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 09:44:37 2017
+
+@author: user
+"""
+
+for i in range(20):
+    print(i)
